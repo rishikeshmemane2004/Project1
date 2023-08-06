2f74c9f62adac264d406ab1040b08b84cd6f2096
rishikeshmemane2004 <rishimemane@gmail.com>
2023-08-06 20:59:11 +0530

    Revert "aaaa"
    This reverts commit 9a532dbf844a2c7d927525142e8be8af6bcef890.

--- Project1/src/test/java/tests/TestClass2.java
+++
@@ -1,9 +0,0 @@
-package tests;
-
-public class TestClass2 {
-
-	public static void main(String[] args) {
-		System.out.println("This is my secondd code with git hub !!!");
-	}
-
-}
--- Project1/src/test/java/tests/TestClass3.java
+++
@@ -1,7 +0,0 @@
-package tests;
-
-public class TestClass3 {
-public static void main(String[] args) {
-	System.out.println("This class is for JIRA story#343456");
-}
-}
