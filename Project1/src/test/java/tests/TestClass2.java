9044bca9a4e6fe0628a4d97ed882dd0d37930094
rishikeshmemane2004 <rishimemane@gmail.com>
2023-08-06 20:56:21 +0530

    kkkk

---
+++ Project1/src/test/java/tests/TestClass2.java
@@ -0,0 +1,9 @@
+package tests;
+
+public class TestClass2 {
+
+	public static void main(String[] args) {
+		System.out.println("This is my secondd code with git hub !!!");
+	}
+
+}
---
+++ Project1/src/test/java/tests/TestClass3.java
@@ -0,0 +1,7 @@
+package tests;
+
+public class TestClass3 {
+public static void main(String[] args) {
+	System.out.println("This class is for JIRA story#343456");
+}
+}
